9b39fcbff954562edcbb807817d19dbd6734f4c6
NKaye <nino.kaye@gmail.com>
2021-11-25 13:00:27 +0000

    Add files via upload

---
+++ baby_snake solution.py
@@ -0,0 +1,27 @@
+from operator import add,xor,ne,eq
+
+# O=chr
+# OO=ord
+# O0=input
+# OOO=add
+# O0O=xor
+# OO0=int
+# O00=str().join
+# O0O0=printSs
+# OO00=ne
+# O000=range
+
+1st=input('a:')
+2nd=int(input('b:')
+print(str().join([chr(xor(ord(1st[n]), 2nd)) for n in range(32))])
+if ne(2nd*4,124):raise
+if ne(1st[17:26],'@l,i,mjl@'):raise
+list=[6,11,12,17,26,31]
+for n in range(0, 6, 2): 
+	if ne(1st[list[n]:list[n+1]],'lq~tz'):raise
+if ne(int(1st[11]),2):raise
+
+#O00O = n
+#list = OO00O0
+#1st restrictions =   AAAAAAlq~tz2lq~tz@l,i,mjl@lq~tzo
+#2nd restrictions = 31
